253591b2e447be894048218c0e587fabd5faa8b4
hsy118 <hsy118@hotmail.co.kr>
2021-06-21 22:36:32 +0900

    solved problem

--- publy_1.js
+++ publy_1.js
@@ -4,6 +4,7 @@ function solution(office, k) {
   for (let i = 0; i < N - k + 1; i++) {
     for (let j = 0; j < N - k + 1; j++) {
       // 1 개수 확인 4중.. 16만가지
+      // 시간을 줄이기 위해
       let value = 0;
       for (let x = 0; x < k; x++) {
         for (let y = 0; y < k; y++) {
